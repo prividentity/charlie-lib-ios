b3b14bbbb9f889c0563095e608b10e76d882d331
Roman <seadller@gmail.com>
2024-12-19 16:40:19 +0200

    Update Predict and Enrol structs

--- Sources/CryptonetPackage/Models/Public/InputModels.swift
+++ Sources/CryptonetPackage/Models/Public/InputModels.swift
@@ -35,16 +35,59 @@ public struct EstimageAgeConfig: Codable {
 public struct PredictConfig: Codable {
     public let imageFormat: String
     public let skipAntispoof: Bool
+    public let mfToken: String?
+    public let antiSpoofingThreshold: Double
+    public let thresholdUserTooClose: Double
+    public let thresholdUserTooFar: Double
+    public let thresholdUserRight: Double
+    public let thresholdUserLeft: Double
+    public let thresholdDownVerticalPredict: Double
+    public let thresholdHighVerticalPredict: Double
+    public let thresholdProfilePredict: Double
+    public let mfCountOverride: Double
+    public let disablePredictMf: Bool
     
     public init(imageFormat: String = "rgba",
-         skipAntispoof: Bool = true) {
+                skipAntispoof: Bool = true, mfToken: String? = nil,
+                antiSpoofingThreshold: Double = 0.75,
+                thresholdUserTooClose: Double = 0.8,
+                thresholdUserTooFar: Double = 0.1,
+                thresholdUserRight: Double = 0.01,
+                thresholdUserLeft: Double = 0.99,
+                thresholdDownVerticalPredict: Double = 0.7,
+                thresholdHighVerticalPredict: Double = -0.1,
+                thresholdProfilePredict: Double = 0.7,
+                mfCountOverride: Double = 3,
+                disablePredictMf: Bool = false) {
         self.imageFormat = imageFormat
         self.skipAntispoof = skipAntispoof
+        self.mfToken = mfToken
+        self.antiSpoofingThreshold = antiSpoofingThreshold
+        self.thresholdUserTooClose = thresholdUserTooClose
+        self.thresholdUserTooFar = thresholdUserTooFar
+        self.thresholdUserRight = thresholdUserRight
+        self.thresholdUserLeft = thresholdUserLeft
+        self.thresholdDownVerticalPredict = thresholdDownVerticalPredict
+        self.thresholdHighVerticalPredict = thresholdHighVerticalPredict
+        self.thresholdProfilePredict = thresholdProfilePredict
+        self.mfCountOverride = mfCountOverride
+        self.disablePredictMf = disablePredictMf
     }
     
     enum CodingKeys: String, CodingKey {
         case imageFormat = "input_image_format"
         case skipAntispoof = "skip_antispoof"
+        case mfToken = "mf_token"
+        case antiSpoofingThreshold = "anti_spoofing_threshold"
+        case thresholdUserTooClose = "threshold_user_too_close"
+        case thresholdUserTooFar = "threshold_user_too_far"
+        case thresholdUserRight = "threshold_user_right"
+        case thresholdUserLeft = "threshold_user_left"
+        case thresholdDownVerticalPredict = "threshold_down_vertical_predict"
+        case thresholdHighVerticalPredict = "threshold_high_vertical_predict"
+        case thresholdProfilePredict = "threshold_profile_predict"
+        case mfCountOverride = "mf_count_override"
+        case disablePredictMf = "disable_predict_mf"
     }
 }
 
@@ -52,19 +95,43 @@ public struct EnrollConfig: Codable {
     public let imageFormat: String
     public let mfToken: String?
     public let skipAntispoof: Bool
+    public let disableEnrollMF: Bool
+    public let thresholdUserTooFar: Double
+    public let thresholdUserTooClose: Double
+    public let thresholdUserRight: Double
+    public let thresholdUserLeft: Double
+    public let thresholdProfileEnroll: Double
     
     public init(imageFormat: String = "rgba",
-         mfToken: String? = nil,
-         skipAntispoof: Bool = true) {
+                mfToken: String? = nil,
+                skipAntispoof: Bool = true,
+                disableEnrollMF: Bool = false,
+                thresholdUserTooFar: Double = 0.31,
+                thresholdUserTooClose: Double = 0.55,
+                thresholdUserRight: Double = 0.2,
+                thresholdUserLeft: Double = 0.8,
+                thresholdProfileEnroll: Double = 0.66) {
         self.imageFormat = imageFormat
         self.mfToken = mfToken
         self.skipAntispoof = skipAntispoof
+        self.disableEnrollMF = disableEnrollMF
+        self.thresholdUserTooFar = thresholdUserTooFar
+        self.thresholdUserTooClose = thresholdUserTooClose
+        self.thresholdUserRight = thresholdUserRight
+        self.thresholdUserLeft = thresholdUserLeft
+        self.thresholdProfileEnroll = thresholdProfileEnroll
     }
     
     enum CodingKeys: String, CodingKey {
         case imageFormat = "input_image_format"
         case skipAntispoof = "skip_antispoof"
         case mfToken = "mf_token"
+        case disableEnrollMF = "disable_enroll_mf"
+        case thresholdUserTooFar = "threshold_user_too_far"
+        case thresholdUserTooClose = "threshold_user_too_close"
+        case thresholdUserRight = "threshold_user_right"
+        case thresholdUserLeft = "threshold_user_left"
+        case thresholdProfileEnroll = "threshold_profile_enroll"
     }
 }
 
